45ba3596da8e25c6fba1e5a2122638581711fb0c
Samuel Vannier <samuel.vannier@etudiant.univ-lr.fr>
2022-03-18 11:38:23 +0100

    feat: implements basic listing with CRUD fonctionalities

--- app/models/Tache.java
+++ app/models/Tache.java
@@ -2,6 +2,11 @@ package models;
 
 import play.db.jpa.Model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Tache extends Model {
-	
+
+    public String title;
+    public boolean done = false;
 }
