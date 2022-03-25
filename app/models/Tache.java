22dd95473669e7bab876b38cad23037b3fcee02f
byhalf <gareth.lafay@gmail.com>
2022-03-25 10:46:27 +0100

    time

--- app/models/Tache.java
+++ app/models/Tache.java
@@ -3,11 +3,12 @@ package models;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
+import java.time.LocalDateTime;
 
 @Entity
 public class Tache extends Model {
 
     public String title;
     public boolean done = false;
-    public LocalDateTime time = new LocalDateTime().now().plusMinutes(1);
+    public LocalDateTime time =  LocalDateTime.now().plusMinutes(1);
 }
