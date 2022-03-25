2626905711f3bdbb71dbea00c46e726e66691208
byhalf <gareth.lafay@gmail.com>
2022-03-25 08:59:01 +0100

    app

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,44 +1,51 @@
 package controllers;
 
+import models.Tache;
 import play.mvc.Controller;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 public class Application extends Controller {
 
     // Affiche toutes les tâches (voir variable taches) dans le template views/listTache.html
     public static void listTache() {
-        // A COMPLETER
-        // ...
-		render();
+        List<Tache> taches = Tache.findAll();
+        render(taches);
     }
 
     // Affiche le template views/ajouterTacheForm.html (formulaire d'ajout d'une tâche)
     public static void ajouterTacheForm() {
-        // A COMPLETER
-        // ...
+        render();
     }
 
     // Ajoute une nouvelle tâche en base de données et affiche le template views/ajouterTache.html
-    public static void ajouterTache() {
-        // A COMPLETER
-        // ...
+    public static void ajouterTache(Tache tache) {
+        tache.save();
+        render();
     }
 
     // Change le statut d'une tâche en base de données
     public static void validerTache(Long id) {
-    	// A COMPLETER
-        // ...   
+        Tache tache = Tache.findById(id);
+        tache.done = !tache.done;
+        tache.save();
+        renderJSON(tache);
     }
 
     // Supprime une tâche en base de données
     public static void supprimerTache(Long id) {
-        // A COMPLETER
-        // ...
+        Tache tache = Tache.findById(id);
+        tache.delete();
+        renderJSON(tache);
     }
 
     // Modifie une tâche en base de données
     public static void editTache(Long id, String title) {
-        // A COMPLETER
-        // ...
+        Tache tache = Tache.findById(id);
+        tache.title = title;
+        tache.save();
+        renderJSON(tache);
     }
 
 }
--- app/models/Tache.java
+++ app/models/Tache.java
@@ -1,7 +1,11 @@
-package models;
-
 import play.db.jpa.Model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Tache extends Model {
-	
+
+
+    public String title;
+    public boolean done = false;
 }
